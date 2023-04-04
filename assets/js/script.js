2d314d142861f95f2bd1588ab7024295213b4eec
brandon6457 <brandon6457@gmail.com>
2023-04-04 18:10:21 -0400

    made changes and added to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,50 @@
+var questions = [{
+   question: "What is used to store multiple values in a single variable?",
+   choices: ["Array", "String", "Variable", "Window"],
+   answer: "Array"
+},{
+   question: "Commonly used data types do not include what?",
+   choices: ["Strings", "Numbers", "Booleans", "Alerts"],
+   answer: "Alerts"
+},{
+   question: "What is the proper way to write a function?",
+   choices: ["Function{}", "Function[]", "Function()", "(function))"],
+   answer: "Function()"
+},{
+   question: "How do you output a message to the web console?",
+   choices: ["Control", "Log.this", "Console.log", "Console"],
+   answer: "Console.log"
+}]
 var quizIntro = document.querySelector(".quiz-intro");
 var btnStart = document.querySelector("#start");
 var questions = document.querySelector("#questions");
 var choices = document.querySelector("#choices");
 var quizEnd = document.querySelector("#quiz-end");
-var currentQuestionIndex = 0;
+btnStart.onclick = startQuiz;
 function startQuiz() {
    quizIntro.style.display = "none";
-   getQuestions()
+   getQuestions();
 }
 function getQuestions() {
    var currentQuestion = questions[currentQuestionIndex];
    var questionTitle = document.querySelector("#question-title");
-   questionTitle.textContent = currentQuestion.question;
+   var currentQuestionIndex = 0;
+   questionTitle.textContent = currentQuestion;
+   currentQuestionIndex = 0;
 }
-   //Create event listener for hs-screen back button to return to main page
+var timeEl = document.querySelector(".time");
 
-   //Create event listener for clear highscores button to clear all scores
+var secondsLeft = 70;
 
-   //Create event listener for start quiz button to shw second page and start quiz
+function startTimer() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
 
-   //Create questions for quiz
-   btnStart.onclick = startQuiz;
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+    }
+
+  }, 1000);
+}
